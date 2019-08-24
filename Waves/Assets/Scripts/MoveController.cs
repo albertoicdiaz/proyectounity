6a5a580fda8d42dd0df23ec09107049c28991c05
Alberto <albertoicdiaz@gmail.com>
2019-08-24 15:31:40 -0400

    mejoras en agua de laberito y floatpoints en basura

---
+++ Waves/Assets/Scripts/MoveController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MoveController : MonoBehaviour
+{
+    float speed = 10F;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 acc = Input.acceleration;
+        rb.AddForce(acc.x * speed, 0, acc.y * speed);
+    }
+}
