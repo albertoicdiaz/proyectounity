70e2bcb8e2dc0ef857ee401da771dfc9e538caa4
Alberto <albertoicdiaz@gmail.com>
2019-08-26 04:50:29 -0400

    cambios en tamaños, en scripts y en modelos

--- Clean Ocean/Assets/Scripts/WavesFollowLaberynth.cs
+++ Clean Ocean/Assets/Scripts/WavesFollowLaberynth.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class WavesFollowLaberynth : MonoBehaviour
 {
-    private GameObject boatPrefab,positionObject;
+    public GameObject boatPrefab,positionObject;
     // Start is called before the first frame update
     void Start()
     {
-        boatPrefab = GameObject.Find("/BoatPrefab");
-        positionObject = GameObject.Find("Position");
+        //boatPrefab = GameObject.Find("/BoatPrefab");
+        //positionObject = GameObject.Find("Position");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(boatPrefab.GetComponent<Transform>().position.x-72, positionObject.GetComponent<Transform>().position.y, boatPrefab.GetComponent<Transform>().position.z-100);
+        transform.position = new Vector3(boatPrefab.GetComponent<Transform>().position.x-33, positionObject.GetComponent<Transform>().position.y, boatPrefab.GetComponent<Transform>().position.z-54);
     }
 }
--- Clean Ocean/Assets/Scripts/deployObjects.cs
+++ Clean Ocean/Assets/Scripts/deployObjects.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class deployObjects : MonoBehaviour {
-    public GameObject objectPrefab, boatPrefab;
+    public GameObject objectPrefab, boatPrefab, Trash;
     public float respawnTime = 1.0f;
     //private Vector2 screenBounds;
-    private int x,count;
-    private float z,b;
+    private int x,count,trashtype;
+    private float z;
 
     // Use this for initialization
     void Start () {
@@ -17,12 +17,71 @@ public class deployObjects : MonoBehaviour {
         count = 1;
     }
     private void spawnTrash(){
-        b=boatPrefab.GetComponent<Transform>().position.z;
-        Debug.Log(b);
-        GameObject a = Instantiate(objectPrefab) as GameObject;
+        //b=boatPrefab.GetComponent<Transform>().position.z;
+        //Debug.Log(b);
         x = Random.Range(-22, 120);
-        //Debug.Log(x);
-        a.transform.position = new Vector3(x, 1,z);
+        trashtype = Random.Range(1, 10);
+        if (trashtype == 1)
+        {
+            Trash = GameObject.Find("/BarrilPrefab");
+            Trash.transform.position = new Vector3(x, 1, z);
+        }
+
+        if (trashtype == 2)
+        {
+            Trash = GameObject.Find("/BasuraPrefab");
+            Trash.transform.position = new Vector3(x, 1, z);
+        }
+
+        if (trashtype == 3)
+        {
+            Trash = GameObject.Find("/BotellaPrefab");
+            Trash.transform.position = new Vector3(x, 1, z);
+        }
+
+        if (trashtype == 4)
+        {
+            Trash = GameObject.Find("/BottlePrefab");
+            Trash.transform.position = new Vector3(x, 1, z);
+        }
+
+        if (trashtype == 5)
+        {
+            Trash = GameObject.Find("/LataPrefab");
+            Trash.transform.position = new Vector3(x, 1, z);
+        }
+        if (trashtype == 6)
+        {
+            Trash = GameObject.Find("/LechePrefab");
+            Trash.transform.position = new Vector3(x, 1, z);
+        }
+
+        if (trashtype == 7)
+        {
+            Trash = GameObject.Find("/PastilleroPrefab");
+            Trash.transform.position = new Vector3(x, 1, z);
+        }
+
+        if (trashtype == 8)
+        {
+            Trash = GameObject.Find("/RuedaPrefab");
+            Trash.transform.position = new Vector3(x, 1, z);
+        }
+
+        if (trashtype == 9)
+        {
+            Trash = GameObject.Find("/SprayPrefab");
+            Trash.transform.position = new Vector3(x, 1, z);
+        }
+
+        if (trashtype == 10)
+        {
+            Trash = GameObject.Find("/VasoPrefab");
+            Trash.transform.position = new Vector3(x, 1, z);
+        }
+        //GameObject a = Instantiate(objectPrefab) as GameObject;
+        ////Debug.Log(x);
+        //a.transform.position = new Vector3(x, 1,z);
         z += 10;
     }
 
