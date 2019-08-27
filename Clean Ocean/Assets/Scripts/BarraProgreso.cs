fb2fe86ee9d7071147a907388890865451180ab3
Alberto <albertoicdiaz@gmail.com>
2019-08-27 05:06:43 -0400

    botones de ui en actividad suma

---
+++ Clean Ocean/Assets/Scripts/BarraProgreso.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarraProgreso : MonoBehaviour
+{
+    Slider Barra;
+
+    public float max;
+    public int act;
+    public Text ValorString;
+
+    // Start is called before the first frame update
+
+    void Awake(){
+        Barra = GetComponent<Slider> ();
+    }
+
+    void Update(){
+        ActualizarValorBarra (max, act);
+    }
+    void ActualizarValorBarra(float ValorMax, float ValorAct ){
+        float porcentaje;
+        porcentaje = ValorAct / ValorMax;
+        Barra.value = porcentaje;
+        ValorString.text = porcentaje*100 + "%";
+    }
+}
